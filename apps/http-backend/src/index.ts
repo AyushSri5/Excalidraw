575b1ff4632a9c057889491be8815c02f2b2885f
AyushSri5 <156398511+AyushSri5@users.noreply.github.com>
2025-01-19 00:33:52 +0530

    Excalidraw Backend

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,14 +1,15 @@
 import express from "express";
 import jwt from "jsonwebtoken";
-import { JWT_SECRET } from "./config";
+import { JWT_SECRET } from "@repo/backend-common/config";
 import { middleware } from "./middleware";
+import {CreateUserSchema} from "@repo/common/types"
 
 const app = express();
 
 app.use(express.json());
 
 app.post('/signup', (req, res) => {
-
+    const data = CreateUserSchema.safeParse(req.body);
 })
 
 app.post('/signin',(req,res) => {
